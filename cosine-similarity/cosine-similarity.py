c67c0e769596b7bd866ab1be25cdefa53ab4796a
Samuel Lee <137744681+SamuelWLee27@users.noreply.github.com>
2026-07-29 16:10:15 +1200

    Solve cosine-similarity

---
+++ cosine-similarity/cosine-similarity.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def cosine_similarity(a, b):
+    """
+    Compute cosine similarity between two 1D NumPy arrays.
+    Returns: float in [-1, 1]
+    """
+    # Write code here
+    a = np.array(a)
+    b = np.array(b)
+    dot_product = np.dot(a, b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    if np.all(norm_a == 0) or np.all(norm_b == 0):
+        return 0
+    return float(dot_product / (norm_a * norm_b))
